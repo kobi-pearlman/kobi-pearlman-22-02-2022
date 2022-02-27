881b0eebe1887c44c37aa55bca4425935c049c03
kobi-pearlman <yakovp6@gmail.com>
2022-02-27 20:32:33 +0200

    change url

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
-export const DEFAULT_URL = "http://dataservice.accuweather.com";
+export const DEFAULT_URL = "https://dataservice.accuweather.com";
 export const API_KEY = "w2cq3qnpVgVPSu2QeA1j60bOQGSuVISP";
 export const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
