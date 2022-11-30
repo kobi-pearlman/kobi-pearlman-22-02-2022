27cfc0fc24fd51c6e87cc02cc50f2c01b21aad96
kobi-pearlman <54657011+kobi-pearlman@users.noreply.github.com>
2022-11-30 15:30:22 +0200

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
 export const DEFAULT_URL = "https://dataservice.accuweather.com";
-export const API_KEY = "Iw33LAb2gEKQcRfCsQGYshR7qgVixwuY";
+export const API_KEY = "aAwJiMDGvgJ0GLsPI7qNuLX9Equl24wL";
 export const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 export const PROJECT_URL = "kobi-pearlman-22-02-2022";
